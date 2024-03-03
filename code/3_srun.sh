dc8a0d8e02e6a11757ca292ff4005888105d5be5
bkellman <benjamin.kellman@gmail.com>
2024-03-03 14:33:17 -0500

    reanalysis

--- code/3_srun.sh
+++ code/3_srun.sh
@@ -3,8 +3,8 @@
 #SBATCH -t 3-00:05                         # Runtime in D-HH:MM format
 #SBATCH -p medium                           # Partition to run in
 #SBATCH --mem=100G                         # Memory total in MiB (for all cores)
-#SBATCH -o hostname_opt_%j.out                 # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e hostname_opt_%j.err                 # File to which STDERR will be written, including job ID (%j)
+#SBATCH -o opt_%j.out                 # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e opt_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
 
 # You can change hostname to any command you would like to run
